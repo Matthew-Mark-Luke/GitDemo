72fda09d270ce52fd4fd11581499e8fa65a47b87
Music <appiumtesting2025@gmail.com>
2025-07-02 22:14:27 -0700

    Architect development

---
+++ GithubLearning/src/test/java/com/github/Test2.java
@@ -0,0 +1,12 @@
+package com.github;
+
+public class Test2 {
+
+	public void Method1_Test2 () {
+		
+		System.out.println("new class Test2");
+		System.out.println("new class Test2");
+		System.out.println("new class Test2");
+		System.out.println("new class Test2");
+	}
+}
