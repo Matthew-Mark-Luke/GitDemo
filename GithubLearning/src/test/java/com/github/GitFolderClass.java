48e9f3491ea37cf3aaca71474e07d2bbc039d189
Music <appiumtesting2025@gmail.com>
2025-07-02 22:32:49 -0700

    GitFolder-GitFolderClass

---
+++ GithubLearning/src/test/java/com/github/GitFolderClass.java
@@ -0,0 +1,14 @@
+package com.github;
+
+public class GitFolderClass {
+
+	public void gitFolder_Method1 () {
+		
+		System.out.println("gitFolder_Method1");
+		System.out.println("gitFolder_Method1");
+		System.out.println("gitFolder_Method1");
+		System.out.println("gitFolder_Method1");
+		System.out.println("gitFolder_Method1");
+		
+	}
+}
